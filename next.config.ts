14e52e4d778684b230b25d788ef4d4427f77a9e8
Simfort <lemondzavadavid265@gmail.com>
2025-11-09 17:13:26 +0300

    fix depricated functions and change on remotePatterns next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["static.hitmcdn.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "static.hitmcdn.com",
+        pathname: "**", // разрешает все пути
+      },
+    ],
   },
 };
 
